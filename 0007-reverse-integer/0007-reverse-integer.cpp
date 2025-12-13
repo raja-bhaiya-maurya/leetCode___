1d38c98d4cb22beb3e058d031fa70e9598f8301e
Raja Bhaiya Maurya <rajabhaiyamaurya390@gmail.com>
2025-12-13 23:10:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,20 +2,16 @@
 class Solution {
   public:        
 int reverse(long long x) {
-      int digit=0;long long rev=0;
-       if(x>=INT_MIN  && x<=INT_MAX){
+ 
+
+   long rev=0;int New;
+                 while(x!=0){
+                New=x%10;
+                rev=rev*10+New;
+                x/=10;}
          
-           while(x!=0){
-                digit=x%10;
-                rev=rev*10+digit;
-                x/=10;
-            }
 
-       }else 
-             return 0;
-
-             if (rev>=INT_MIN  && rev<=INT_MAX)
-        return rev;
-        else return 0;
+        
+       if(rev>=INT_MIN && rev<=INT_MAX )return rev;else return 0; 
     }
         };
