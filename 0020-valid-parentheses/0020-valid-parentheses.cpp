154817743b99195aa9a1187737a3675705ab4a80
Raja Bhaiya Maurya <rajabhaiyamaurya390@gmail.com>
2026-02-17 00:12:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.98%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char>st;
+         for(int i=0;i<s.length();i++){
+                if(s[0]==')'||s[0]=='}'||s[0]==']') return false;
+                if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                    st.push(s[i]);
+                }else{
+                    if(st.empty())return false;
+                    if(s[i]==')'&& st.top()=='('){
+                        st.pop();
+                    }else if(s[i]==']'&& st.top()=='['){
+                        st.pop();
+                    }else if(s[i]== '}' && st.top()=='{'){
+                        st.pop();
+                    }else return false;
+                    
+                }
+            }
+            if(st.empty()) return true ;else return false;
+        
+    }
+};
