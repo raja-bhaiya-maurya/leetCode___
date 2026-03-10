feb4f6a422e9bde1b615861ba7bd40bbb18c803a
Raja Bhaiya Maurya <rajabhaiyamaurya390@gmail.com>
2026-03-10 22:06:46 +0530

    Time: 0 ms (100%), Space: 35.8 MB (53.65%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int total = 0;
+
+        for(int num : nums){
+            total += num;
+        }
+
+        int left = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+
+            if(left == total - left - nums[i]){
+                return i;
+            }
+
+            left += nums[i];
+        }
+
+        return -1;
+    }
+};
